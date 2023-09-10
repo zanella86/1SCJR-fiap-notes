f5fd8f8f5bfdac017b28dd18b973b226fba0d28f
zanella86 <thiago_zanella@yahoo.com.br>
2023-09-10 15:36:26 -0300

    Update apiAuth.ts
    Alterando o endpoint de autenticação

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const apiAuth = axios.create({
-  baseURL: `https://fiap-notes-api-auth.herokuapp.com/`,
+  //baseURL: `https://fiap-notes-api-auth.herokuapp.com/`,
+  baseURL: `https://legendary-chainsaw-g4wvxp5jxg6c9jg5-3333.app.github.dev/`,
 });
